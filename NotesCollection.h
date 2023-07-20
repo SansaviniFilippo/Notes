4adea86da08c18019d31f01f43bb316f9e8df408
Sansavini Filippo <filippo.sansavini@stud.unifi.it>
2023-07-20 18:02:22 +0200

    created Note.cpp

---
+++ NotesCollection.h
@@ -0,0 +1,27 @@
+//
+// Created by filippo on 20/07/23.
+//
+
+#ifndef NOTES_NOTESCOLLECTION_H
+#define NOTES_NOTESCOLLECTION_H
+
+#include <iostream>
+#include <map>
+#include <utility>
+#include "Note.h"
+
+class NotesCollection {
+public:
+    NotesCollection(std::string n) : name(std::move(n)) {}
+    void addNote(Note note);
+    void removeNote(Note note);
+    void printAllNotesTitle();
+    void printAllNotes();
+    std::string getName() const;
+    void setName(std::string n);
+private:
+    std::string name;
+    std::map<std::string, Note> collection;
+};
+
+#endif //NOTES_NOTESCOLLECTION_H
