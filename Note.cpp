a31f7dc77fc55035d0503f659e645ae05e748b62
Sansavini Filippo <filippo.sansavini@stud.unifi.it>
2023-07-20 17:46:08 +0200

    created Note.cpp

--- Note.cpp
+++ Note.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "Note.h"
+
+#include <utility>
+
+std::string Note::getTitle() const {
+    return title;
+}
+
+std::string Note::getText() const {
+    return text;
+}
+
+bool Note::isBlocked() const {
+    return blocked;
+}
+
+void Note::setTitle(std::string ti) {
+    title = std::move(ti);
+}
+
+void Note::setText(std::string te) {
+    text = std::move(te);
+}
+
+void Note::setBlocked(bool bl) {
+    blocked = bl;
+}
