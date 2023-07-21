b49b22c76a8430158b08699062fedcceb4a620bc
Sansavini Filippo <filippo.sansavini@stud.unifi.it>
2023-07-21 21:32:31 +0200

    created class Subject.h

---
+++ Subject.h
@@ -0,0 +1,18 @@
+//
+// Created by filippo on 21/07/23.
+//
+
+#ifndef NOTES_SUBJECT_H
+#define NOTES_SUBJECT_H
+
+#include "Observer.h"
+
+class Subject {
+public:
+    virtual void subscribe(Observer* o) = 0;
+    virtual void unsubscribe(Observer* o) = 0;
+    virtual void notify() = 0;
+    virtual ~Subject() = default;
+};
+
+#endif //NOTES_SUBJECT_H
