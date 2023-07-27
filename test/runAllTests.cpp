ab65c1afea6877fff471abf9d3f8e3e6cb1a6417
Sansavini Filippo <filippo.sansavini@stud.unifi.it>
2023-07-27 18:20:09 +0200

    created runAllTest.cpp

---
+++ test/runAllTests.cpp
@@ -0,0 +1,10 @@
+//
+// Created by filippo on 25/07/23.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
