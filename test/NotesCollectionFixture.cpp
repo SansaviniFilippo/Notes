ab3bfe1cc97ed5e59ceb59bb7dc3a12feffdd440
Sansavini Filippo <filippo.sansavini@stud.unifi.it>
2023-07-27 17:59:17 +0200

    Add update, print and blocked test in Notes collection's test

--- test/NotesCollectionFixture.cpp
+++ test/NotesCollectionFixture.cpp
@@ -23,11 +23,34 @@ TEST_F(NotesCollectionTest, AddNoteTest) {
     EXPECT_EQ(collection->getNoteNumber(), 2);
 }
 
-TEST_F(NotesCollectionTest, RemoveNoteTest) {
+TEST_F(NotesCollectionTest, PrintAndRemoveTest) {
     auto note1 = std::make_shared<Note>("Note 1", "Text 1");
     auto note2 = std::make_shared<Note>("Note 2", "Text 2");
     collection->addNote(note1);
     collection->addNote(note2);
+    collection->printOneNotes(note1);
+    collection->printAllNotes();
+    collection->printAllNotesTitle();
     collection->removeNote(note1);
     EXPECT_EQ(collection->getNoteNumber(), 1);
+    collection->printAllNotesTitle();
+}
+
+TEST_F(NotesCollectionTest, UpdateTest) {
+    auto note1 = std::make_shared<Note>("Note 1", "Text 1");
+    collection->addNote(note1);
+    collection->editNoteTitle(note1, "New Note 1");
+    EXPECT_EQ(note1->getTitle(), "New Note 1");
+    collection->editNoteText(note1, "New Text 1");
+    EXPECT_EQ(note1->getText(), "New Text 1");
+}
+
+TEST_F(NotesCollectionTest, BlockedTest) {
+    auto note1 = std::make_shared<Note>("Note 1", "Text 1", false);
+    collection->addNote(note1);
+    EXPECT_FALSE(note1->isBlocked());
+    collection->block(note1);
+    EXPECT_TRUE(note1->isBlocked());
+    collection->unblock(note1);
+    EXPECT_FALSE(note1->isBlocked());
 }
