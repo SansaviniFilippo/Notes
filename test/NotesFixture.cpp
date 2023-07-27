f09c087a0dab2d70182df77e09f098392c174ca1
Sansavini Filippo <filippo.sansavini@stud.unifi.it>
2023-07-27 17:37:33 +0200

    Created Notes's test

---
+++ test/NotesFixture.cpp
@@ -0,0 +1,27 @@
+//
+// Created by filippo on 25/07/23.
+//
+
+#include "gtest/gtest.h"
+#include "../Note.h"
+
+TEST(NotesFixture, GetterAndSetterTest) {
+    Note note("Title", "Text", false);
+    EXPECT_EQ(note.getTitle(), "Title");
+    EXPECT_EQ(note.getText(), "Text");
+    EXPECT_FALSE(note.isBlocked());
+
+    note.setTitle("New Title");
+    note.setText("New Text");
+    note.setBlocked(true);
+    EXPECT_EQ(note.getTitle(), "New Title");
+    EXPECT_EQ(note.getText(), "New Text");
+    EXPECT_TRUE(note.isBlocked());
+}
+
+TEST(NotesFixture, EmptyInitializationTest) {
+    Note note("","");
+    EXPECT_EQ(note.getTitle(), "");
+    EXPECT_EQ(note.getText(), "");
+    EXPECT_FALSE(note.isBlocked());
+}
